e104048cf8d158e527c4b8bbe24f12dd3ee32b12
Farzana-Bhuiyan <farzanabhuiyancse,cu@gmail.com>
2018-12-06 23:10:21 +0600

    Add evaluation file

---
+++ evaluation.py
@@ -0,0 +1,9 @@
+import json
+from typing import List, Dict
+from collections import Counter
+
+import numpy as np
+from sklearn.metrics import cohen_kappa_score
+import matplotlib.pyplot as plt
+
+from questions import Questions
